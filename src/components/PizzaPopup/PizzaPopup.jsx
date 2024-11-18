db6142174bdbd82b824bc6ea8ba7c152a6d1658d
Nikita <belka.nik02@gmail.com>
2024-11-18 13:31:03 +0200

    Доделал попап

--- src/components/PizzaPopup/PizzaPopup.jsx
+++ src/components/PizzaPopup/PizzaPopup.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { PizzaSizeSelector } from '../PizzaSizeSelector/PizzaSizeSelector';
 import css from './PizzaPopup.module.css';
 import { RiCloseLargeLine } from 'react-icons/ri';
+import { addToCart } from '../../redux/cartSlice';
 
 export function PizzaPopup({ pizza, onClose }) {
+  const [activeSizeIndex, setActiveSizeIndex] = useState(0); // Размер по умолчанию
+  const dispatch = useDispatch();
+
+  const sizePriceAdditions = [0, 110]; // Разница в цене для разных размеров
+  const finalPrice = pizza.price + sizePriceAdditions[activeSizeIndex];
+
+  const handleAddToCart = () => {
+    dispatch(
+      addToCart({
+        id: pizza.id,
+        title: pizza.title,
+        imageUrl: pizza.imageUrl,
+        size: pizza.sizes[activeSizeIndex],
+        price: finalPrice,
+      }),
+    );
+    // onClose(); // Закрываем попап после добавления в корзину
+  };
+
   return (
-    <div className={css.popupOverlay}>
-      <div className={css.popupContent}>
+    <div className={css.popupOverlay} onClick={onClose}>
+      <div
+        className={css.popupContent}
+        onClick={(e) => e.stopPropagation()} // Останавливаем всплытие
+      >
         <button onClick={onClose} className={css.closeButton}>
           <RiCloseLargeLine />
         </button>
@@ -16,6 +42,11 @@ export function PizzaPopup({ pizza, onClose }) {
         <div className={css.popupContentBlockInfo}>
           <h2 className={css.popupContentBlockInfoNamePizza}>{pizza.title}</h2>
           <div className={css.popupContentBlockInfoList}>
+            <PizzaSizeSelector
+              sizes={pizza.sizes}
+              activeSizeIndex={activeSizeIndex}
+              onSizeClick={setActiveSizeIndex}
+            />
             <h6>Ингредиенты:</h6>
             <ul className={css.popupContentBlockInfoListIngredients}>
               {pizza.ingredients.map((ingredientImage, index) => (
@@ -25,6 +56,9 @@ export function PizzaPopup({ pizza, onClose }) {
                 </li>
               ))}
             </ul>
+            <button onClick={handleAddToCart} className={css.pizzaBlockAddToCart}>
+              В корзину за {finalPrice} грн.
+            </button>
           </div>
         </div>
       </div>
